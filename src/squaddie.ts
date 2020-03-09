0d6256434ed16ee68677df0213f2f0d5a1b49cd5
Chad Serrant <therealchadius@gmail.com>
2020-03-09 13:04:43 -0400

    Add Squaddie strength.

--- src/squaddie.ts
+++ src/squaddie.ts
@@ -1,15 +1,33 @@
 export class Squaddie {
   maxHealth: number
+  strength: number
 
-  constructor(maxHealth) {
+  constructor(maxHealth, strength = 0) {
     this.maxHealth = maxHealth
+    this.strength = strength
+  }
+
+  getBaseMaxHealth() {
+    return this.maxHealth
   }
 
   getMaxHealth() {
-    return this.maxHealth
+    return this.getBaseMaxHealth()
+  }
+
+  getCurrentHealth() {
+    return this.getHealth()
   }
 
   getHealth() {
     return this.maxHealth
+  }
+
+  getBaseStrength() {
+    return this.strength
+  }
+
+  getCurrentStrength() {
+    return this.getBaseStrength()
   }
 }
--- test/squaddieAttack.ts
+++ test/squaddieAttack.ts
@@ -5,10 +5,15 @@ describe('Squaddie creation', () => {
   it('Has health', () => {
     const soldier = new Squaddie(5)
 
+    expect(soldier.getBaseMaxHealth()).to.equal(5)
     expect(soldier.getMaxHealth()).to.equal(5)
+    expect(soldier.getHealth()).to.equal(5)
+    expect(soldier.getCurrentHealth()).to.equal(5)
+  })
 
-    function getHealth() {
-      expect(soldier.getHealth()).to.equal(5)
-    }
+  it('Has Strength', () => {
+    const soldier = new Squaddie(5, 3)
+    expect(soldier.getBaseStrength()).to.equal(3)
+    expect(soldier.getCurrentStrength()).to.equal(3)
   })
 })
