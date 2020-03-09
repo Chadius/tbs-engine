ebc35020afa42b1bb6b84b9eb9578fbde1081aaa
Chad Serrant <therealchadius@gmail.com>
2020-03-08 20:42:26 -0400

    Verify map has same of rows and columns.

--- src/terrainTypes.ts
+++ src/terrainTypes.ts
@@ -2,6 +2,11 @@ export class TerrainTypeMap {
   tileTypesByRow: Array<string[]>
 
   constructor(typeTiles: Array<string[]>) {
+    const numColumnsInEachRow = typeTiles.map((row: string[]) => row.length)
+    if (numColumnsInEachRow.every(col => col === numColumnsInEachRow[0]) !== true) {
+      throw TypeError("All rows must have the same number of columns.")
+    }
+
     this.tileTypesByRow = typeTiles
   }
 
--- test/terrainType.ts
+++ test/terrainType.ts
@@ -1,24 +1,36 @@
-import {TerrainTypeMap} from "../src/terrainTypes"
+import {TerrainTypeMap} from '../src/terrainTypes'
 import {expect} from 'chai'
 
-describe ("Terrain Dimensions are determined by Blocks", () => {
-  it("Knows the number of rows based on the initialization", () => {
+describe ('Terrain Dimensions are determined by Blocks', () => {
+  it('Knows the number of rows based on the initialization', () => {
     const terrain = new TerrainTypeMap([
-      ["1", "1"],
-      ["1", "X"],
-      ["3", "1"],
+      ['1', '1'],
+      ['1', 'X'],
+      ['3', '1'],
     ])
 
     expect(terrain.rowCount()).to.eq(3)
   })
 
-  it("Knows the number of columns based on the initialization", () => {
+  it('Knows the number of columns based on the initialization', () => {
     const terrain = new TerrainTypeMap([
-      ["1", "1", "2"],
-      ["1", "X", "X"],
-      ["S", "1", "X"],
+      ['1', 'X', 'X'],
+      ['S', '1', 'X'],
     ])
 
     expect(terrain.columnCount()).to.eq(3)
+  })
+})
+
+describe('Invalid terrain definition throws exceptions', () => {
+  it('Throws an error when the rows do not have the same number of tiles', () => {
+    const makeBadTerrain = () => {
+      new TerrainTypeMap([
+        [],
+        ['1', '2'],
+      ])
+    }
+
+    expect(makeBadTerrain).to.throw(TypeError)
   })
 })
