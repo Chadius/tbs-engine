6acda15250e242c98b3b5821deeddfb73e40a9b1
Chad Serrant <therealchadius@gmail.com>
2020-03-22 15:59:03 -0400

    Search strategies use composition.

--- src/searchStrategies/SquaddieOneTurnMovementStrategy.ts
+++ src/searchStrategies/SquaddieOneTurnMovementStrategy.ts
@@ -1,13 +1,59 @@
-import {Path} from "../mapMeasurement";
-import {SearchStrategy, SquaddieSearchHistoryContext} from "./SearchStrategy";
-import {StraightLineStrategy} from "./StraightLineStrategy";
+import {Coordinate, Path} from "../mapMeasurement";
+import {SearchHistoryContext, SearchStrategy, SquaddieSearchHistoryContext} from "./SearchStrategy";
+import {
+  addNeighborsToPathAndCreateNewPaths,
+  addPathLocationToVisited,
+  checkIfCoordinatesAreOffMap, endIfPathIsAtDestination, getUnvisitedCoordinatesNextToPathHead,
+  initalizeSearchHistoryWithPriorityQueue, morePathsToSearch, popNextPathFromQueue,
+} from "./StraightLineStrategy";
 
-export class SquaddieOneTurnMovementStrategy extends StraightLineStrategy implements SearchStrategy {
-  addNewPathsToSearch(searchHistoryContext: SquaddieSearchHistoryContext, newPaths: Array<Path>): void {
-    newPaths.forEach((newPath) => {
-      if (newPath.getMovementCostSpent() <= searchHistoryContext.squaddie.getCurrentMovePerTurn()) {
-        searchHistoryContext.pathsToSearch.push(newPath)
-      }
-    })
+const addNewPathsIfWithinMoverange = (searchHistoryContext: SquaddieSearchHistoryContext, newPaths: Array<Path>): void => {
+  newPaths.forEach((newPath) => {
+    if (newPath.getMovementCostSpent() <= searchHistoryContext.squaddie.getCurrentMovePerTurn()) {
+      searchHistoryContext.pathsToSearch.push(newPath)
+    }
+  })
+}
+
+
+export class SquaddieOneTurnMovementStrategy implements SearchStrategy {
+  checkForEarlyExitCondition(searchHistoryContext: SearchHistoryContext): {shouldExitEarly: boolean; returnVal: null} {
+    return checkIfCoordinatesAreOffMap(searchHistoryContext)
   }
+
+  initalizeSearchHistory(searchHistoryContext: SearchHistoryContext): void {
+    return initalizeSearchHistoryWithPriorityQueue(searchHistoryContext)
+  }
+
+  addNewPathsToSearch(searchHistoryContext: SquaddieSearchHistoryContext, newPaths: Array<Path>): void {
+    return addNewPathsIfWithinMoverange(searchHistoryContext, newPaths)
+  }
+
+  shouldContinueSearching(searchHistoryContext: SearchHistoryContext): boolean {
+    return morePathsToSearch(searchHistoryContext)
+  }
+
+  getNextPath(searchHistoryContext: SearchHistoryContext): Path {
+    return popNextPathFromQueue(searchHistoryContext)
+  }
+
+  markPathAsVisited(searchHistoryContext: SearchHistoryContext, currentPath: Path): void {
+    return addPathLocationToVisited(searchHistoryContext, currentPath)
+  }
+
+  shouldEndSearchEarly(searchHistoryContext: SearchHistoryContext, currentPath: Path): {shouldExitEarly: boolean; returnVal: Path} {
+    return endIfPathIsAtDestination(searchHistoryContext, currentPath)
+  }
+
+  findNewNeighborsForPath(searchHistoryContext: SearchHistoryContext, currentPath: Path): Array<Coordinate> {
+    return getUnvisitedCoordinatesNextToPathHead(searchHistoryContext, currentPath)
+  }
+
+  addNeighborsToPath(searchHistoryContext: SearchHistoryContext, neighbors: Array<Coordinate>, currentPath: Path): Array<Path> {
+    return addNeighborsToPathAndCreateNewPaths(searchHistoryContext, neighbors, currentPath)
+  }
+
+  noPathsRemain(searchHistoryContext: SearchHistoryContext): any {
+    return null
+  };
 }
--- src/searchStrategies/StraightLineStrategy.ts
+++ src/searchStrategies/StraightLineStrategy.ts
@@ -14,72 +14,108 @@ export const lowerMoveCostIsFirst = (a: Path, b: Path): number => {
   return 0
 }
 
+export const checkIfCoordinatesAreOffMap = (searchHistoryContext: SearchHistoryContext): {shouldExitEarly: boolean; returnVal: null} => {
+  const {battleMap, startCoordinate, endCoordinate} = searchHistoryContext
+  if (battleMap.isOnMap(startCoordinate) && battleMap.isOnMap(endCoordinate)) {
+    return { shouldExitEarly: false, returnVal: undefined }
+  }
+  return { shouldExitEarly: true, returnVal: null }
+}
+
+export const initalizeSearchHistoryWithPriorityQueue = (searchHistoryContext: SearchHistoryContext): void => {
+  const {startCoordinate, battleMap, visitedLocations} = searchHistoryContext
+
+  searchHistoryContext.pathsToSearch = new TinyQueue<Path> ([], lowerMoveCostIsFirst)
+  searchHistoryContext.pathsToSearch.push(new Path(startCoordinate))
+  visitedLocations.clear()
+  visitedLocations.add(
+    battleMap.coordinatesToLocationIndex(startCoordinate)
+  )
+}
+
+export const morePathsToSearch = (searchHistoryContext: SearchHistoryContext): boolean => {
+  return (searchHistoryContext.pathsToSearch.length > 0)
+}
+
+export const popNextPathFromQueue = (searchHistoryContext: SearchHistoryContext): Path => {
+  return searchHistoryContext.pathsToSearch.pop()
+}
+
+export const addPathLocationToVisited = (searchHistoryContext: SearchHistoryContext, currentPath: Path): void => {
+  const currentPathLocationIndex = searchHistoryContext.battleMap.coordinatesToLocationIndex(
+    currentPath.getCurrentCoordinates()
+  )
+
+  searchHistoryContext.visitedLocations.add(currentPathLocationIndex)
+}
+
+export const endIfPathIsAtDestination = (searchHistoryContext: SearchHistoryContext, currentPath: Path): {shouldExitEarly: boolean; returnVal: Path} => {
+  const pathIsAtDestination = (currentPath.getCurrentCoordinates().equals(searchHistoryContext.endCoordinate))
+  if (pathIsAtDestination) {
+    return {shouldExitEarly: true, returnVal: currentPath}
+  }
+  return {shouldExitEarly: false, returnVal: undefined }
+}
+
+export const getUnvisitedCoordinatesNextToPathHead = (searchHistoryContext: SearchHistoryContext, currentPath: Path): Array<Coordinate> => {
+  const neighbors = searchHistoryContext.battleMap.getNeighbors(currentPath.getCurrentCoordinates())
+    .filter((neighbor) => {
+      const locationIndex = searchHistoryContext.battleMap.coordinatesToLocationIndex(neighbor)
+      return !(searchHistoryContext.visitedLocations.has(locationIndex))
+    })
+  return neighbors
+}
+
+export const addNeighborsToPathAndCreateNewPaths = (searchHistoryContext: SearchHistoryContext, neighbors: Array<Coordinate>, currentPath: Path): Array<Path> => {
+  return neighbors.map((neighbor) => {
+    const movementCostToNeighbor = 1
+    const newPathToSearch = currentPath.clone()
+    newPathToSearch.addCoordinate(neighbor, movementCostToNeighbor)
+    return newPathToSearch
+  })
+}
+
+export const addNewPathsToSearchQueue = (searchHistoryContext: SearchHistoryContext, newPaths: Array<Path>): void => {
+  newPaths.forEach((newPath) => {
+    searchHistoryContext.pathsToSearch.push(newPath)
+  })
+}
+
 export class StraightLineStrategy implements SearchStrategy {
   checkForEarlyExitCondition(searchHistoryContext: SearchHistoryContext): {shouldExitEarly: boolean; returnVal: null} {
-    const {battleMap, startCoordinate, endCoordinate} = searchHistoryContext
-    if (battleMap.isOnMap(startCoordinate) && battleMap.isOnMap(endCoordinate)) {
-      return { shouldExitEarly: false, returnVal: undefined }
-    }
-    return { shouldExitEarly: true, returnVal: null }
+    return checkIfCoordinatesAreOffMap(searchHistoryContext)
   }
 
   initalizeSearchHistory(searchHistoryContext: SearchHistoryContext): void {
-    const {startCoordinate, battleMap, visitedLocations} = searchHistoryContext
-
-    searchHistoryContext.pathsToSearch = new TinyQueue<Path> ([], lowerMoveCostIsFirst)
-    searchHistoryContext.pathsToSearch.push(new Path(startCoordinate))
-    visitedLocations.clear()
-    visitedLocations.add(
-      battleMap.coordinatesToLocationIndex(startCoordinate)
-    )
+    return initalizeSearchHistoryWithPriorityQueue(searchHistoryContext)
   }
 
   shouldContinueSearching(searchHistoryContext: SearchHistoryContext): boolean {
-    return (searchHistoryContext.pathsToSearch.length > 0)
+    return morePathsToSearch(searchHistoryContext)
   }
 
   getNextPath(searchHistoryContext: SearchHistoryContext): Path {
-    return searchHistoryContext.pathsToSearch.pop()
+    return popNextPathFromQueue(searchHistoryContext)
   }
 
   markPathAsVisited(searchHistoryContext: SearchHistoryContext, currentPath: Path): void {
-    const currentPathLocationIndex = searchHistoryContext.battleMap.coordinatesToLocationIndex(
-      currentPath.getCurrentCoordinates()
-    )
-
-    searchHistoryContext.visitedLocations.add(currentPathLocationIndex)
+    return addPathLocationToVisited(searchHistoryContext, currentPath)
   }
 
   shouldEndSearchEarly(searchHistoryContext: SearchHistoryContext, currentPath: Path): {shouldExitEarly: boolean; returnVal: Path} {
-    const pathIsAtDestination = (currentPath.getCurrentCoordinates().equals(searchHistoryContext.endCoordinate))
-    if (pathIsAtDestination) {
-      return {shouldExitEarly: true, returnVal: currentPath}
-    }
-    return {shouldExitEarly: false, returnVal: undefined }
+    return endIfPathIsAtDestination(searchHistoryContext, currentPath)
   }
 
   findNewNeighborsForPath(searchHistoryContext: SearchHistoryContext, currentPath: Path): Array<Coordinate> {
-    const neighbors = searchHistoryContext.battleMap.getNeighbors(currentPath.getCurrentCoordinates())
-      .filter((neighbor) => {
-        const locationIndex = searchHistoryContext.battleMap.coordinatesToLocationIndex(neighbor)
-        return !(searchHistoryContext.visitedLocations.has(locationIndex))
-      })
-    return neighbors
+    return getUnvisitedCoordinatesNextToPathHead(searchHistoryContext, currentPath)
   }
 
   addNeighborsToPath(searchHistoryContext: SearchHistoryContext, neighbors: Array<Coordinate>, currentPath: Path): Array<Path> {
-    return neighbors.map((neighbor) => {
-      const movementCostToNeighbor = 1
-      const newPathToSearch = currentPath.clone()
-      newPathToSearch.addCoordinate(neighbor, movementCostToNeighbor)
-      return newPathToSearch
-    })
+    return addNeighborsToPathAndCreateNewPaths(searchHistoryContext, neighbors, currentPath)
   }
 
   addNewPathsToSearch(searchHistoryContext: SearchHistoryContext, newPaths: Array<Path>): void {
-    newPaths.forEach((newPath) => {
-      searchHistoryContext.pathsToSearch.push(newPath)
-    })
+    return addNewPathsToSearchQueue(searchHistoryContext, newPaths)
   }
 
   noPathsRemain(searchHistoryContext: SearchHistoryContext): any {
