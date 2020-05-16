509e100770803fe0e52a3bd6734d04aa2391266c
Chad Serrant <therealchadius@gmail.com>
2020-05-16 16:46:08 -0400

    Remove camera controls.

--- src/battleScene/battleSceneBottomLayer.ts
+++ src/battleScene/battleSceneBottomLayer.ts
@@ -1,13 +1,9 @@
 import "phaser";
-import {ZoomInfoForBattleMainLayer} from "../battleSceneUtilities/zoomLevelManagement";
-import {CameraControlForMainLayer} from "../battleSceneUtilities/cameraControlManagement";
 
 export class BattleSceneBottomLayer {
   rowHeight = 64
   tileWidth = 64
   scene: Phaser.Scene
-  zoomInfo: ZoomInfoForBattleMainLayer
-  cameraInfo: CameraControlForMainLayer
   mainLayerBounds = {x: 0, y: 0, width: 800, height: 600}
 
   constructor(scene: Phaser.Scene) {
@@ -32,14 +28,7 @@ export class BattleSceneBottomLayer {
     graphics.strokeLineShape(new Phaser.Geom.Line(0,  0, 800, 0));
   }
 
-  centerCamera(x: number, y: number) {
-    this.scene.cameras.main.setZoom(1.0)
-    this.scene.cameras.main.setScroll(x, y)
-  }
-
   update(time: number, delta: number): void {
-    this.updateCameraZoom(delta)
-    this.updateCameraScroll(delta)
   }
 
   drawMapLayer() {
@@ -69,111 +58,14 @@ export class BattleSceneBottomLayer {
   }
 
   setupCamera() {
-    this.centerCamera(0, 0)
-    this.createCameraControls()
-    this.zoomInfo = new ZoomInfoForBattleMainLayer({
-      initial: 1.0,
-      min: 1.0 / 4.0,
-      max: 1.0 * 4.0,
-      interpolator: Phaser.Math.Interpolation.Linear,
-    })
-
-    this.cameraInfo = new CameraControlForMainLayer({
-      position: {
-        x: 0,
-        y: 0
-      },
-      scrollSpeedPerSecond: 400,
-      distanceFunction: Phaser.Math.Distance.Between,
-      angleFunction: Phaser.Math.Angle.Between,
-    })
-
-    this.scene.cameras.main.setZoom(this.zoomInfo.getCurrentZoomLevel())
+    this.scene.cameras.main.setScroll(0, 0)
+    this.scene.cameras.main.setZoom(1.0)
 
     this.scene.cameras.main.setBounds(
       this.mainLayerBounds.x,
       this.mainLayerBounds.y,
       this.mainLayerBounds.width,
       this.mainLayerBounds.height
-    )
-  }
-
-  createCameraControls() {
-    this.scene.input.keyboard.on('keydown-MINUS', (event) => {
-      this.zoomOutCamera()
-    })
-    this.scene.input.keyboard.on('keydown-PLUS', (event) => {
-      this.zoomInCamera()
-    })
-
-    this.scene.input.keyboard.on('keydown-DOWN', (event) => {
-      this.scrollCameraDown()
-    })
-    this.scene.input.keyboard.on('keydown-UP', (event) => {
-      this.scrollCameraUp()
-    })
-    this.scene.input.keyboard.on('keydown-LEFT', (event) => {
-      this.scrollCameraLeft()
-    })
-    this.scene.input.keyboard.on('keydown-RIGHT', (event) => {
-      this.scrollCameraRight()
-    })
-  }
-
-  zoomInCamera() {
-    if (this.zoomInfo.isInTransition()) {
-      return
-    }
-    this.zoomInfo.transitionZoomLevel(this.zoomInfo.getCurrentZoomLevel() * 2.0)
-  }
-
-  zoomOutCamera() {
-    if (this.zoomInfo.isInTransition()) {
-      return
-    }
-    this.zoomInfo.transitionZoomLevel(this.zoomInfo.getCurrentZoomLevel() / 2.0)
-  }
-
-  updateCameraZoom(timeDelta: number) {
-    this.zoomInfo.passMilliseconds(timeDelta)
-    this.scene.cameras.main.setZoom(this.zoomInfo.getCurrentZoomLevel())
-  }
-
-  scrollCameraUp() {
-    const currentPosition = this.cameraInfo.getPosition()
-    this.scrollCameraWithinBounds(currentPosition[0], currentPosition[1] - 100)
-  }
-
-  private scrollCameraWithinBounds(destinationX: number, destinationY: number) {
-    this.cameraInfo.transitionScroll(
-      [
-        this.scene.cameras.main.clampX(destinationX),
-        this.scene.cameras.main.clampY(destinationY)
-      ]
-    )
-  }
-
-  scrollCameraDown() {
-    const currentPosition = this.cameraInfo.getPosition()
-    this.scrollCameraWithinBounds(currentPosition[0], currentPosition[1] + 100)
-  }
-
-  scrollCameraLeft() {
-    const currentPosition = this.cameraInfo.getPosition()
-    this.scrollCameraWithinBounds(currentPosition[0] - 100, currentPosition[1])
-  }
-
-  scrollCameraRight() {
-    const currentPosition = this.cameraInfo.getPosition()
-    this.scrollCameraWithinBounds(currentPosition[0] + 100, currentPosition[1])
-  }
-
-  updateCameraScroll(timeDelta: number) {
-    this.cameraInfo.passMilliseconds(timeDelta)
-    const currentPosition = this.cameraInfo.getPosition()
-    this.scene.cameras.main.setScroll(
-      currentPosition[0],
-      currentPosition[1],
     )
   }
 }
