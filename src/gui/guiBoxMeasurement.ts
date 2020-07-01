4ae5603b3d225a87b216a5000252be1da17c05fd
Chad Serrant <therealchadius@gmail.com>
2020-06-30 20:05:33 -0400

    Gui box measurement can calculate padding margin and border.

--- src/gui/guiBoxMeasurement.ts
+++ src/gui/guiBoxMeasurement.ts
@@ -6,12 +6,83 @@ export class GuiBoxMeasurement {
     height: number;
   }
 
-  constructor(param: { origin: { top: number; left: number; width: number; height: number } }) {
+  margin: {
+    top: number;
+    right: number;
+    bottom: number;
+    left: number;
+  }
+
+  border: {
+    top: number;
+    right: number;
+    bottom: number;
+    left: number;
+  }
+
+  padding: {
+    top: number;
+    right: number;
+    bottom: number;
+    left: number;
+  }
+
+  constructor(param: {
+    origin: { top: number; left: number; width: number; height: number };
+    margin?: { top: number; right: number; bottom: number; left: number };
+    border?: { top: number; right: number; bottom: number; left: number };
+    padding?: { top: number; right: number; bottom: number; left: number };
+  }) {
     this.origin = {
       left: param.origin.left,
       top: param.origin.top,
       width: param.origin.width,
       height: param.origin.height,
+    }
+
+    this.margin = {
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0,
+    }
+    if (param.margin) {
+      this.margin = {
+        top: param.margin.top,
+        right: param.margin.right,
+        bottom: param.margin.bottom,
+        left: param.margin.left,
+      }
+    }
+
+    this.border = {
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0,
+    }
+    if (param.border) {
+      this.border = {
+        top: param.border.top,
+        right: param.border.right,
+        bottom: param.border.bottom,
+        left: param.border.left,
+      }
+    }
+
+    this.padding = {
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0,
+    }
+    if (param.padding) {
+      this.padding = {
+        top: param.padding.top,
+        right: param.padding.right,
+        bottom: param.padding.bottom,
+        left: param.padding.left,
+      }
     }
   }
 
@@ -37,36 +108,161 @@ export class GuiBoxMeasurement {
   setOriginLeft(left: number) {
     this.origin.left = left
   }
-
   setOriginTop(top: number) {
     this.origin.top = top
   }
-
   setOriginWidth(width: number) {
     if (width < 0) {
       width = 0
     }
     this.origin.width = width
   }
-
   setOriginHeight(height: number) {
     if (height < 0) {
       height = 0
     }
     this.origin.height = height
   }
-
   setOriginRight(right: number) {
     if (right < this.getOriginLeft()) {
       right = this.getOriginLeft()
     }
     this.origin.width = right - this.origin.left
   }
-
   setOriginBottom(bottom: number) {
     if (bottom < this.getOriginTop()) {
       bottom = this.getOriginTop()
     }
     this.origin.height = bottom - this.origin.top
+  }
+
+  getMarginTopValue(): number {
+    return this.margin.top
+  }
+  getMarginRightValue(): number {
+    return this.margin.right
+  }
+  getMarginBottomValue(): number {
+    return this.margin.bottom
+  }
+  getMarginLeftValue(): number {
+    return this.margin.left
+  }
+
+  getMarginTopEnd(): number {
+    return this.getOriginTop() + this.getMarginTopValue()
+  }
+  getMarginRightEnd(): number {
+    return this.getOriginRight() - this.getMarginRightValue()
+  }
+  getMarginBottomEnd(): number {
+    return this.getOriginBottom() - this.getMarginBottomValue()
+  }
+  getMarginLeftEnd(): number {
+    return this.getOriginLeft() + this.getMarginLeftValue()
+  }
+
+  setMarginTop(top: number) {
+    this.margin.top = top
+  }
+  setMarginRight(right: number) {
+    this.margin.right = right
+  }
+  setMarginBottom(bottom: number) {
+    this.margin.bottom = bottom
+  }
+  setMarginLeft(left: number) {
+    this.margin.left = left
+  }
+
+  getBorderTopValue(): number {
+    return this.border.top
+  }
+  getBorderRightValue(): number {
+    return this.border.right
+  }
+  getBorderBottomValue(): number {
+    return this.border.bottom
+  }
+  getBorderLeftValue(): number {
+    return this.border.left
+  }
+
+  getBorderTopEnd(): number {
+    return this.getMarginTopEnd() + this.getBorderTopValue()
+  }
+  getBorderRightEnd(): number {
+    return this.getMarginRightEnd() - this.getBorderRightValue()
+  }
+  getBorderBottomEnd(): number {
+    return this.getMarginBottomEnd() - this.getBorderBottomValue()
+  }
+  getBorderLeftEnd(): number {
+    return this.getMarginLeftEnd() + this.getBorderLeftValue()
+  }
+
+  setBorderTop(top: number) {
+    this.border.top = top
+  }
+  setBorderRight(right: number) {
+    this.border.right = right
+  }
+  setBorderBottom(bottom: number) {
+    this.border.bottom = bottom
+  }
+  setBorderLeft(left: number) {
+    this.border.left = left
+  }
+
+  getPaddingTopValue(): number {
+    return this.padding.top
+  }
+  getPaddingRightValue(): number {
+    return this.padding.right
+  }
+  getPaddingBottomValue(): number {
+    return this.padding.bottom
+  }
+  getPaddingLeftValue(): number {
+    return this.padding.left
+  }
+
+  getPaddingTopEnd(): number {
+    return this.getBorderTopEnd() + this.getPaddingTopValue()
+  }
+  getPaddingRightEnd(): number {
+    return this.getBorderRightEnd() - this.getPaddingRightValue()
+  }
+  getPaddingBottomEnd(): number {
+    return this.getBorderBottomEnd() - this.getPaddingBottomValue()
+  }
+  getPaddingLeftEnd(): number {
+    return this.getBorderLeftEnd() + this.getPaddingLeftValue()
+  }
+
+  setPaddingTop(top: number) {
+    this.padding.top = top
+  }
+  setPaddingRight(right: number) {
+    this.padding.right = right
+  }
+  setPaddingBottom(bottom: number) {
+    this.padding.bottom = bottom
+  }
+  setPaddingLeft(left: number) {
+    this.padding.left = left
+  }
+
+  getContentTop(): number {
+    return this.getPaddingTopEnd()
+  }
+  getContentRight(): number {
+    return this.getPaddingRightEnd()
+  }
+  getContentBottom(): number {
+    return this.getPaddingBottomEnd()
+  }
+  getContentLeft(): number {
+    return this.getPaddingLeftEnd()
   }
 }
--- test/gui/guiBoxMeasurements.ts
+++ test/gui/guiBoxMeasurements.ts
@@ -1,15 +1,45 @@
 import {expect} from 'chai'
 import {GuiBoxMeasurement} from "../../src/gui/guiBoxMeasurement";
 
-describe("Create GUI Box Measurements", () => {
-  it("Knows the overall dimensions", () => {
+describe('Create GUI Box Measurements', () => {
+  let origin
+  let margin
+  let border
+  let padding
+
+  beforeEach(() => {
+    origin = {
+      left: 100,
+      top: 10,
+      width: 200,
+      height: 120,
+    }
+
+    margin = {
+      top: 20,
+      right: 10,
+      bottom: 15,
+      left: 0,
+    }
+
+    border = {
+      top: 10,
+      right: 10,
+      bottom: 10,
+      left: 10,
+    }
+
+    padding = {
+      top: 20,
+      right: 20,
+      bottom: 20,
+      left: 20,
+    }
+  })
+
+  it('Knows the origin dimensions', () => {
     const window = new GuiBoxMeasurement({
-      origin : {
-        left: 100,
-        top: 10,
-        width: 200,
-        height: 120
-      }
+      origin : origin
     })
 
     expect(window.getOriginLeft()).to.eql(100)
@@ -19,10 +49,82 @@ describe("Create GUI Box Measurements", () => {
     expect(window.getOriginRight()).to.eql(300)
     expect(window.getOriginBottom()).to.eql(130)
   })
+
+  it('Knows the margin', () => {
+    const window = new GuiBoxMeasurement({
+      origin : origin,
+      margin: margin,
+    })
+
+    expect(window.getMarginTopValue()).to.eql(margin.top)
+    expect(window.getMarginRightValue()).to.eql(margin.right)
+    expect(window.getMarginBottomValue()).to.eql(margin.bottom)
+    expect(window.getMarginLeftValue()).to.eql(margin.left)
+
+    expect(window.getMarginTopEnd()).to.eql(30)
+    expect(window.getMarginRightEnd()).to.eql(290)
+    expect(window.getMarginBottomEnd()).to.eql(115)
+    expect(window.getMarginLeftEnd()).to.eql(100)
+  })
+
+  it('Knows the border', () => {
+    const window = new GuiBoxMeasurement({
+      origin : origin,
+      margin: margin,
+      border: border,
+    })
+
+    expect(window.getBorderTopValue()).to.eql(border.top)
+    expect(window.getBorderRightValue()).to.eql(border.right)
+    expect(window.getBorderBottomValue()).to.eql(border.bottom)
+    expect(window.getBorderLeftValue()).to.eql(border.left)
+
+    expect(window.getBorderTopEnd()).to.eql(40)
+    expect(window.getBorderRightEnd()).to.eql(280)
+    expect(window.getBorderBottomEnd()).to.eql(105)
+    expect(window.getBorderLeftEnd()).to.eql(110)
+  })
+
+  it('Knows the padding', () => {
+    const window = new GuiBoxMeasurement({
+      origin : origin,
+      margin: margin,
+      border: border,
+      padding: padding,
+    })
+
+    expect(window.getPaddingTopValue()).to.eql(padding.top)
+    expect(window.getPaddingRightValue()).to.eql(padding.right)
+    expect(window.getPaddingBottomValue()).to.eql(padding.bottom)
+    expect(window.getPaddingLeftValue()).to.eql(padding.left)
+
+    expect(window.getPaddingTopEnd()).to.eql(60)
+    expect(window.getPaddingRightEnd()).to.eql(260)
+    expect(window.getPaddingBottomEnd()).to.eql(85)
+    expect(window.getPaddingLeftEnd()).to.eql(130)
+  })
+
+  it('Knows the bounding box for the content', () => {
+    const window = new GuiBoxMeasurement({
+      origin : origin,
+      margin: margin,
+      border: border,
+      padding: padding,
+    })
+
+    expect(window.getContentTop()).to.eql(60)
+    expect(window.getContentRight()).to.eql(260)
+    expect(window.getContentBottom()).to.eql(85)
+    expect(window.getContentLeft()).to.eql(130)
+  })
 })
 
 describe("Change a GUI Box Measurements", () => {
   let origin
+  let margin
+  let border
+  let padding
+
   let window: GuiBoxMeasurement
 
   beforeEach(() => {
@@ -31,6 +133,27 @@ describe("Change a GUI Box Measurements", () => {
       top: 10,
       width: 200,
       height: 120
+    }
+
+    margin = {
+      top: 20,
+      right: 10,
+      bottom: 15,
+      left: 0,
+    }
+
+    border = {
+      top: 10,
+      right: 10,
+      bottom: 10,
+      left: 10,
+    }
+
+    padding = {
+      top: 20,
+      right: 20,
+      bottom: 20,
+      left: 20,
     }
 
     window = new GuiBoxMeasurement({
@@ -73,5 +196,47 @@ describe("Change a GUI Box Measurements", () => {
     expect(window.getOriginTop()).to.eql(200)
     expect(window.getOriginBottom()).to.eql(200)
 
+  })
+
+  it("Can change the margins", () => {
+    window.setMarginTop(30)
+    expect(window.getMarginTopEnd()).to.eql(40)
+
+    window.setMarginRight(50)
+    expect(window.getMarginRightEnd()).to.eql(250)
+
+    window.setMarginBottom(30)
+    expect(window.getMarginBottomEnd()).to.eql(100)
+
+    window.setMarginLeft(25)
+    expect(window.getMarginLeftEnd()).to.eql(125)
+  })
+
+  it("Can change the border", () => {
+    window.setBorderTop(20)
+    expect(window.getBorderTopEnd()).to.eql(30)
+
+    window.setBorderRight(20)
+    expect(window.getBorderRightEnd()).to.eql(280)
+
+    window.setBorderBottom(20)
+    expect(window.getBorderBottomEnd()).to.eql(110)
+
+    window.setBorderLeft(20)
+    expect(window.getBorderLeftEnd()).to.eql(120)
+  })
+
+  it("Can change the padding", () => {
+    window.setPaddingTop(50)
+    expect(window.getPaddingTopEnd()).to.eql(60)
+
+    window.setPaddingRight(50)
+    expect(window.getPaddingRightEnd()).to.eql(250)
+
+    window.setPaddingBottom(50)
+    expect(window.getPaddingBottomEnd()).to.eql(80)
+
+    window.setPaddingLeft(50)
+    expect(window.getPaddingLeftEnd()).to.eql(150)
   })
 })
